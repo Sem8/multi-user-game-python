7e8929cff5ecdb9dbad47854a9e712565a710f33
Sem8 <mssemmi8@gmail.com>
2019-07-27 18:50:49 -0400

    made the game multi round using two different while loops and coded the logic to reset health points once a new round is started

--- game.py
+++ game.py
@@ -1,24 +1,47 @@
-player = {'name': 'Manuel', 'attack': 13, 'heal': 16, 'health': 100}
-monster = {'name': 'Max', "attack": 12, 'health': 100}
 game_running = True
 
 while game_running == True:
+    new_round = True
 
-    print('Please select action')
-    print('1) Attack')
-    print('2) Heal')
+    player = {'name': 'Manuel', 'attack': 13, 'heal': 16, 'health': 100}
+    monster = {'name': 'Max', "attack": 12, 'health': 100}
 
-    player_choice = input()
+    print('---' * 7)
+    print('Enter Player name')
+    player['name'] = input()
 
-    if player_choice == "1":
-        monster['health'] = monster['health'] - player['attack']
-        player['health'] = player['health'] - monster['attack']
+    while new_round == True:
 
-        print(monster['health'])
-        print(player['health'])
-        
-    elif player_choice == '2':
-        print('Heal Player')
+        player_won = False
+        monster_won = False
 
-    if player['health'] <= 0 or monster['health'] <= 0:
-        game_running = False
+        print('---' * 7)
+        print('Please select action')
+        print('1) Attack')
+        print('2) Heal')
+        print('3) Exit Game')
+
+        player_choice = input()
+
+        if player_choice == "1":
+            monster['health'] = monster['health'] - player['attack']
+            if monster['health'] <= 0:
+                player_won = True
+            else:
+                player['health'] = player['health'] - monster['attack']
+                if player['health'] <= 0:
+                    monster_won = True
+
+            print(monster['health'])
+            print(player['health'])
+            
+        elif player_choice == '2':
+            print('Heal Player')
+
+        elif player_choice == '3':            
+            game_running = False
+            new_round = False
+            
+
+        if player_won == True or monster_won == True:
+            new_round = False
